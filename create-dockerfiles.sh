f2885e85b4f240eae49f9d254a436049f2453d10
OKAMURA, Yasunobu <okamura@informationsea.info>
2021-11-24 17:17:23 +0900

    update to GENCODE38

--- create-dockerfiles.sh
+++ create-dockerfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sed -e 's/__GENOME_VERSION__/GRCh38.p13/' Dockerfile-template | \
-    sed -e 's/__GENCODE_VERSION__/37/' | sed -e 's/__DOWNLOAD_PREFIX__//' | \
+    sed -e 's/__GENCODE_VERSION__/38/' | sed -e 's/__DOWNLOAD_PREFIX__//' | \
     sed -e 's/__GENCODE_TYPE__/.basic/' \
-    > Dockerfile.GENCODE37.GRCh38.p13.basic
+    > Dockerfile.GENCODE38.GRCh38.p13.basic
 
 sed -e 's/__GENOME_VERSION__/GRCh37.primary_assembly/' Dockerfile-template | \
-    sed -e 's/__GENCODE_VERSION__/37/' | sed -e 's|__DOWNLOAD_PREFIX__|GRCh37_mapping/|' | \
+    sed -e 's/__GENCODE_VERSION__/38/' | sed -e 's|__DOWNLOAD_PREFIX__|GRCh37_mapping/|' | \
     sed -e 's/__GENCODE_TYPE__/lift37.basic/' \
-    > Dockerfile.GENCODE37.GRCh37.primary_assembly.basic
+    > Dockerfile.GENCODE38.GRCh37.primary_assembly.basic
